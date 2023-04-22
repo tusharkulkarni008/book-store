5036a634ef843d5c9bc91eeb10871c3b041c8e7b
Chanakya Kulkarni <tusharkulkarni95@gmail.com>
2023-04-22 13:55:47 +0530

    login and signup using local storage done

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,46 +1,62 @@
-import React, { Component } from 'react'
-export default class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
+import { useState } from 'react';
+
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    if (userData && userData.email === email && userData.password === password) {
+      alert('Login Successful');
+    } else {
+      alert('User not registered. Please sign up first.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign In</h3>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input type="checkbox" className="custom-control-input" id="customCheck1" />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
         </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
-    )
-  }
-}
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Forgot <a href="#">password?</a>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,47 +1,71 @@
-import React, { Component } from 'react'
-import {useState} from 'react'
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
-    )
-  }
+import  { useState } from 'react';
+
+function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.log({ firstName, lastName, email, password });
+    const userData = { firstName, lastName, email, password };
+    localStorage.setItem('userData', JSON.stringify(userData));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Sign Up</h3>
+      <div className="mb-3">
+        <label>First name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Last name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Already registered <a href="/sign-in">sign in?</a>
+      </p>
+    </form>
+  );
 }
+
+export default SignUp;
