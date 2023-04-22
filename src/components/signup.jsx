0a64a810fd71c67e9bf83bf24194208ecc2c72fd
Chanakya Kulkarni <tusharkulkarni95@gmail.com>
2023-04-22 11:23:03 +0530

    Update signup.jsx

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import React,{useState} from 'react'
+import {useState} from 'react'
 export default class SignUp extends Component {
   render() {
     return (
